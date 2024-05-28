9d84976dc041e55eea0c378857e561533bc47d4a
GianBaeza <gianb04@gmail.com>
2024-05-28 19:44:37 -0300

    aplicamos buenas practicas, y pude hacer que el fetch sea mas limpio

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 const input = document.querySelector("#searchImg");
 const search = document.querySelector("#search");
-const containerImg = document.querySelector("#gallery");
+const containerImgs = document.querySelector("#gallery");
+const API_KEY = "_ATNM3_x9bOySNge2mZSpKvdW58LCuIF8TYveLyIczY";
 
-
-
-
-
-document.addEventListener("DOMContentLoaded", async function () {
-  const query = input.value.trim();
-  input.focus();
-  if (!query) {
-    await searchmIg("Paisajes");
+search.addEventListener("click", function (event) {
+  event.preventDefault();
+  const valueSearch = input.value.trim();
+  if (valueSearch) {
+    getData(valueSearch);
   } else {
-    await searchmIg(query);
+    const a = "minimalist";
+    getData("paisajes");
   }
 });
-
-search.addEventListener("click", function () {
-  const query = input.value.trim();
-  searchmIg(query);
+cardDiv.addEventListener("mouseover", () => {
+  title.style.opacity = 1; // Mostrar el nombre del auto
+});
+cardDiv.addEventListener("mouseout", () => {
+  title.style.opacity = 0; // Ocultar el nombre del auto
 });
 
-async function searchmIg(query) {
-  const API_KEY = "eySQClLCffh5UwJKLBoHnDFiVrvsHmHrMSqAFz2s6IY4JxgfJvCkgpAG";
-  const apiURL = `https://api.pexels.com/v1/search?query=${query}&per_page=200`;
-
-  try {
-    const response = await fetch(apiURL, {
-      method: "GET",
-      headers: {
-        Accept: "application.json",
-        authorization: API_KEY,
-      },
+function getData(value) {
+  const url = `https://api.unsplash.com/search/photos?&query=${value}&client_id=${API_KEY}`;
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const imgData = data.results.map((item) => {
+        const {
+          id,
+          alt_description,
+          urls: { small: photo },
+          user: { name },
+        } = item;
+        return { id, alt_description, photo, name };
+      });
+      displayImages(imgData);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-    const { photos } = await response.json();
-
-    updateGarelly(photos);
-  } catch (error) {
-    console.error("Error al fetch las imágenes:", error);
-  }
 }
 
-function updateGarelly(photos) {
-  containerImg.innerHTML = " ";
+function displayImages(imgData) {
+  containerImgs.innerHTML = " ";
+  imgData.forEach(({ id, alt_description, photo, name }) => {
+    //containerCard
+    const contianerCardImg = document.createElement("div");
+    containerImgs.classList.add("container-Img");
+    //immagen
+    const cardImg = document.createElement("img");
+    cardImg.src = photo;
+    cardImg.alt = alt_description;
+    cardImg.classList.add("imagenes");
 
-  photos.forEach(({ src, alt }) => {
-    const img = document.createElement("img");
-    img.classList.add("imagenes");
-    img.src = src.portrait;
-    img.dataset.src = src.original;
-    img.alt = alt;
-    img.loading = "lazy";
-    containerImg.appendChild(img);
+    //titleimagen hover
+    const title = document.createElement("p");
+    title.textContent = name;
+    title.classList.add("title-img-hover");
+
+    contianerCardImg.appendChild(cardImg);
+    contianerCardImg.appendChild(title);
+    containerImgs.appendChild(contianerCardImg);
   });
-}
-
-function handleError(error) {
-  console.error("Error fetching images:", error);
-  containerImg.innerHTML =
-    "<p>Ocurrió un error al buscar las imágenes. Por favor, intenta de nuevo.</p>";
 }
